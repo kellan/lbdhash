b58e480ab861172550f6cb34221e395eb61e1d15
kellan <kellan@pobox.com>
2016-05-20 10:31:05 -0400

    start extracting exif

--- Img_Dhash.py
+++ Img_Dhash.py
@@ -4,9 +4,9 @@ import os
 import sys
 import uuid
 from PIL import Image
-import PIL.Image
-import dhash
 import hashlib
+import dhash
+import exif
 
 s3_client = boto3.client('s3')
 
@@ -16,7 +16,7 @@ def handler(event, context):
         #key = record['s3']['object']['key']
 
     bucket = 'kellanio-alestic-lambda-example'
-    key = 'iceberg.jpg'
+    key = 'jazz-thailand.JPEG'
     download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
     upload_path = '/tmp/resized-{}'.format(key)
 
@@ -24,5 +24,6 @@ def handler(event, context):
     image = Image.open(download_path)
     image_dhash = dhash.dhash(image)
     image_sha256 = hashlib.sha256(open(download_path, 'rb').read()).hexdigest()
+    exif_data = exif.get_exif_data(image)
 
-    return {'sha256': image_sha256, 'dhash': image_dhash}
+    return {'sha256': image_sha256, 'dhash': image_dhash, 'exif': exif_data}
