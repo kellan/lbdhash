37c373ea4abab52995a9de321ff0cf31fd3118d4
kellan <kellan@pobox.com>
2016-05-21 16:11:27 -0400

    either invokable directly or via SNS

--- Img_Dhash.py
+++ Img_Dhash.py
@@ -6,20 +6,57 @@ import uuid
 from PIL import Image
 import hashlib
 import pprint
+import logging
+import json
+
 import dhash
 import exif
+import config
 
-s3_client = boto3.client('s3')
+#
+# trigger via SNS with
+#{"default":"SNS requires this",
+#  "bucket":"kellanio-alestic-lambda-example",
+#  "object":"jazz-thailand.JPEG"}
+#
 
 def handler(event, context):
-    #for record in event['Records']:
-        #bucket = record['s3']['bucket']['name']
-        #key = record['s3']['object']['key']
+    logging.getLogger().setLevel(logging.INFO)
+    logging.info('Img_Dhash.handler')
+    logging.info('event {}'.format(event))
 
-    bucket = 'kellanio-alestic-lambda-example'
-    key = 'jazz-thailand.JPEG'
+    if 'Records' in event:
+        for record in event['Records']:
+            logging.info('looking at {}'.format(record))
+            if 'aws:sns' == record['EventSource']:
+                handle_sns(record['Sns'], context)
+            else:
+                logger.info('not an sns event')
+                pprint.pprint(event)
+    elif 'bucket' in event:
+        # invoked directly: aws lambda invoke --function-name Img_DhashPy --region us-west-2 --payload '{"bucket":"kellanio-alestic-lambda-example","key":"jazz-thailand.JPEG"}'
+        return hash_photo(event['bucket'], event['key'])
+
+def handle_sns(sns_event, context):
+    logging.info('handle_sns')
+    bucket, key = sns_event['Message'].split(' ')
+    photo_hash = hash_photo(bucket, key)
+    logging.info('photo_hash {}'.format(photo_hash['dhash']))
+
+    sns = boto3.client('sns')
+    logging.info('attempting to publish')
+
+    sns.publish(
+        TopicArn=config.output_sns_topic,
+        Message=json.dumps(photo_hash))
+
+
+def hash_photo(bucket, key):
+    s3_client = boto3.client('s3')
+
+    #bucket = 'kellanio-alestic-lambda-example'
+    #key = 'jazz-thailand.JPEG'
     download_path = '/tmp/{}{}'.format(uuid.uuid4(), key)
-    upload_path = '/tmp/resized-{}'.format(key)
 
     s3_client.download_file(bucket, key, download_path)
     image = Image.open(download_path)
