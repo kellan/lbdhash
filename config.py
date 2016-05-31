5379c88f7883bb7ef233c2694effa07f0e0ae494
kellan <kellan@pobox.com>
2016-05-31 13:36:10 -0400

    really need a better solution

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-input_sns_topic = 'arn:aws:sns:us-west-2:407883137925:kellaniotopic1'
-output_sns_topic = 'arn:aws:sns:us-west-2:407883137925:Img_Dhash_out'
+input_sns_topic = 'arn:aws:sns:us-east-1:407883137925:Img_Dhash_in'
+output_sns_topic = 'arn:aws:sns:us-east-1:407883137925:Img_Dhash_out'
 dynamo_table = 'kellanio.Medias'
